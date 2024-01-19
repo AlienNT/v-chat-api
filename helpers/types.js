9807cc81f3a0a4265f8c74fac646f9899c64b35d
Trysten M <nick.tristen22@gmail.com>
2024-01-19 22:43:34 +0200

    add (api/helpers): add request types helper

---
+++ helpers/types.js
@@ -0,0 +1,17 @@
+export const msgTypes = {
+    LOGIN: 'login',
+    LOGOUT: 'logout',
+    REGISTRATION: 'registration',
+    CREATE_DIALOG: 'createDialog',
+    GET_DIALOGS: 'getDialogs',
+    DELETE_DIALOG: 'deleteDialog',
+    ADD_MESSAGE: 'addMessage',
+    GET_MESSAGES: 'getMessages',
+    DELETE_MESSAGE: 'deleteMessage',
+    RESPONSE_STATUS: 'responseStatus',
+    GET_PROFILE: 'getProfile',
+    UPDATE_PROFILE: 'updateProfile',
+    GET_SETTINGS: 'getSettings',
+    UPDATE_SETTINGS: 'updateSettings',
+    GET_USER: 'getUser'
+}
